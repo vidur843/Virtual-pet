07255adb7079a4bd7befafe1860e97f83b820967
vidur843 <65580238+vidur843@users.noreply.github.com>
2020-08-06 10:39:09 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,14 @@
+class food{
+  constructor(){
+      //last fed
+     // foodstock=0
+     //images of the food
+
+
+  }
+  //function that will update the foodstock
+  //function to get the feedtime that will be dependant on last fed
+  //function to decrease the count of the food when it is fed to the dog
+  // function to get the stock of the food
+  // need a display function to display the food
+}
